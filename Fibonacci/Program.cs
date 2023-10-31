307ee1df34825326f13c7713818e7d0a2f6a3457
Acer <Acer@DESKTOP-JS0O9TD>
2023-11-01 00:12:13 +0330

    Add project files.

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,14 @@
+﻿//نیما فدوی
+//فیبوناچی
+int a = 0, b = 1, c;
+Console.WriteLine("please enter a number");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"{a}\t{b}\t");
+for (int i = 2; i <= n; i++)
+{
+    c = a + b;
+    Console.Write($"{c}\t");
+    a = b;
+    b = c;
+}
